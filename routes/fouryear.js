194ee2240de5cc1f9464c524d593dc68fa87d068
quincden <78387066+quincden@users.noreply.github.com>
2021-09-15 20:55:50 -0400

    Added a Procfile.

--- routes/fouryear.js
+++ routes/fouryear.js
@@ -21,7 +21,22 @@ router.get("/Policy/:major", verify.verToken, (req, res) => {
 router.get("/FourYearIn/:check", (req, res) => {
     collection = mongoUtil.getFourYear();
     var name = req.params.check;
-    collection.find({'major': name}).project({'major': 1, _id:0}).toArray((error, result) => {
+    collection.findOne({'major': name}).project({'major': 1, _id:0}).toArray((error, result) => {
+        if(error) {
+            return res.status(500).send(error);
+        }
+        if(result == "")
+            res.json(0);
+        else
+            res.json(1);
+    });
+});
+
+//Check if Four Year Plan (Regex Search) exists
+router.get("/FourYearInRegex/:check", (req, res) => {
+    collection = mongoUtil.getFourYear();
+    var name = req.params.check;
+    collection.findOne({"major": {"$regex": maj}}).project({'major': 1, _id:0}).toArray((error, result) => {
         if(error) {
             return res.status(500).send(error);
         }
