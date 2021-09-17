7e27565e98236989e3837afefeeaf4eb428300ee
quincden <78387066+quincden@users.noreply.github.com>
2021-09-16 20:07:46 -0400

    Fixing login

--- routes/Update.js
+++ routes/Update.js
@@ -3,12 +3,29 @@ const mongoUtil = require('../mongoUtil');
 const verify = require('./token');
 var collection;
 
-// getDistinctSchools
+// set majors to updated values
 router.post("/MajorSet", verify.verToken, (req, res) => {
-    var query = req.params.query;
+    var quer = req.params.query;
     var sid = req.params.s_id;
     var maj = req.params.maj;
-    var newVal = { $set: {query: maj}};
+    var newVal = { $set: {quer : maj}};
+    console.log(sid, maj, quer)
+    collection = mongoUtil.getStud();
+    collection.updateOne({"s_id": sid}, newVal,
+    (error, result) => {
+        if(error) {
+            return res.status(500).send(error);
+        }
+        res.send(result);
+        
+    });
+});
+
+// pull outdated majors
+router.post("/MajorPull", verify.verToken, (req, res) => {
+    var sid = req.params.s_id;
+    var newVal = { $pull: {'major': {'title': 'null'}}};
+    console.log(sid)
     collection = mongoUtil.getStud();
     collection.updateOne({"s_id": sid}, newVal,
     (error, result) => {
