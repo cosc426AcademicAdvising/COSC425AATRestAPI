de6d99945eba2747fcd373c4f824da790fb6630d
quincden <78387066+quincden@users.noreply.github.com>
2021-09-18 20:55:47 -0400

    Rollbacked previous login changes
    Reverted the token to be transferred as string instead of json object due to conflicts retrieving token for the website

--- routes/auth.js
+++ routes/auth.js
@@ -40,7 +40,6 @@ router.post('/register', async(req, res) => {
 
 //login
 router.post('/login', async (req, res) => {
-    console.log(req.body);
     collect = mongoUtil.getApiAccess();
     // validate first
     const {error} = loginValidation(req.body);
