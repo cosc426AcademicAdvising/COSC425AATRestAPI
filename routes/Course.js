80facd68f8e180db917c424a6fc4eba74d50b9f3
quincden <78387066+quincden@users.noreply.github.com>
2021-09-28 14:51:19 -0400

    testing regex course query

--- routes/Course.js
+++ routes/Course.js
@@ -26,7 +26,7 @@ router.get("/Course/Regex/:subject/:catalog", (req, res) => {
                 { 'subject': { '$regex': sub} },
                 { 'catalog': { '$regex': cat} }
             ]})
-            .project({'major': 1, _id:0}).toArray((error, result) => {
+            .project({'Long Title': 1, _id:0}).toArray((error, result) => {
         if(error) {
             return res.status(500).send(error);
         }
