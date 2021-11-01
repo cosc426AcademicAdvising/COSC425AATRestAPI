62da8a52ca87bd641d941c3de26cdda0e45baf21
BrianReddersen <reddersenbrian@gmail.com>
2021-11-01 13:26:23 -0700

    Update student.js

--- routes/student.js
+++ routes/student.js
@@ -52,4 +52,26 @@ router.get("/all/students",  verify.verToken, (req, res) => {
         res.send(result);
     });
 });
+
+//posts new student
+router.post("/new/:id", verify.verToken, (req, res) => {
+    collection = mongoUtil.getStud();
+    var sid = parseInt(req.params.id);
+	var name = req.body.name;
+	var passHash = req.body.passHash;
+	var enrll = req.body.enrll;
+	var stud = {};
+	stud[name] = name;
+	stud[passHash] = passHash;
+	stud[enrll] = enrll;
+	var newVal = { $set: stud};
+    collection.insertOne({"s_id": sid}, newVal,
+    (error, result) => {
+        if(error) {
+            return res.status(500).send(error);
+        }
+        res.send(result);
+    });
+});
+
 module.exports = router;
